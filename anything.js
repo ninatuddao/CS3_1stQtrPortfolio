3f6c59ecfa5339169211b0fc733e6a90fecfa85c
ninatuddao <b2029jmdtuddao@pshs.edu.ph>
2025-08-29 13:52:09 +0800

    practice

---
+++ anything.js
@@ -0,0 +1,45 @@
+console.log('BANGIS BANGIS BOY')
+console.log('67 mango')
+// tv5 - 5
+// abscbn - 2
+// gma - 7
+// ibc - 13
+// ptv - 4
+
+rl = require('readline')
+rli = rl.createInterface({
+    input : process.stdin,
+})
+rli.on('line', function(line))
+    if (line !== "sigma"){
+        line !== sigma ? inputs.push(line) : rli.close
+    }
+
+var inputs = []
+
+rli.on('close', get_the_channel)
+
+function get_the_channel(){
+    for (let i=0; i<inputs.length; i++){
+        switch(inputs[i]){
+        line = line.trim(). toLowerCase()
+    switch(line){
+        case "5":
+            console.log("tv5")
+            break;
+        case "2":
+            console.log ("abscbn")
+            break;
+        case "7":
+            console.log ("gma")
+            break;
+        case "13":
+            console.log ("ibc")
+            break;
+        case "4":
+            console.log ("ptv")
+            break;
+        default:
+            console.log("Unknown channel")
+    }
+}}}
